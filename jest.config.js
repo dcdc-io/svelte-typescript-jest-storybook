e88a6d95613da1f60edc3356469951b5d4a2068c
Ben Babik <work@benjaminbabik.com>
2020-08-07 12:43:32 +0100

    jest working

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
   transform: {
-    '^.+\\.svelte$': 'svelte-jester',
+    '^.+\\.svelte$': ['svelte-jester', { "preprocess": true}],
     '^.+\\.js$': 'babel-jest',
   },
   moduleFileExtensions: ['js', 'ts', 'svelte'],
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
 };
